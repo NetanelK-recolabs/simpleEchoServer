a9976ff9a8e9befd2670391a49f5ba5fa2aa6384
Netanel Katzburg <NetanelK@recolabs.ai>
2021-10-19 10:49:28 +0300

    proto, server: Refactor proto message format and server functionality
    proto uses gRPC simple request response.
    proto message does not need timestamp, keep it simple.
    
    Signed-off-by: Netanel Katzburg <NetanelK@recolabs.ai>

--- goServer/server.go
+++ goServer/server.go
@@ -1,19 +1,49 @@
 package main
 
 import (
+    //"context"
     "fmt"
-    "net/http"
+    "log"
+    "net"
+	//"net/http"
+
+    // This import path is based on the name declaration in the go.mod,
+    // and the gen/proto/go output location in the buf.gen.yaml.
+	echov1 "echoapis/echo/v1/gen/proto/go/echo/v1"
+	"google.golang.org/grpc"
 )
 
 /* Simple Go(lang) echo server */
 
 func main() {
+    if err := run(); err != nil {
+        log.Fatal(err)
+    }
+}
+
+func run() error {
+    listenOn := "127.0.0.1:8080"
+    listener, err := net.Listen("tcp", listenOn)
+    if err != nil {
+        return fmt.Errorf("failed to listen on %s: %w", listenOn, err)
+    }
+
+    server := grpc.NewServer()
+    echov1.RegisterEchoServiceServer(server, &echoServiceServer{})
+    log.Println("Listening on", listenOn)
+    if err := server.Serve(listener); err != nil {
+        return fmt.Errorf("failed to serve gRPC server: %w", err)
+    }
+
+    return nil
+}
+
+// echoServiceServer implements the EchoServiceServer API.
+type echoServiceServer struct {
+    echov1.UnimplementedEchoServiceServer
+}
+
+/*func main() {
     http.HandleFunc("/", HelloServer)
     http.ListenAndServe(":8080", nil)
-}
-
-// TODO: change this to echo on client msgs instead of URL path
-func EchoServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-}
-
+}*/
