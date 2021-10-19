65ac3ef224bfcb0ac7c81e16c83b0789189133a4
Netanel Katzburg <NetanelK@recolabs.ai>
2021-10-19 17:10:18 +0300

    client.py: Add grpclib client instead of socket
    Create echo and echo many requests.
    
    Signed-off-by: Netanel Katzburg <NetanelK@recolabs.ai>

--- pyClient/client.py
+++ pyClient/client.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
 
-import socket
-import gen.proto.python.echo.v1.echo_pb2 as echov1
+import asyncio
+from grpclib.client import Channel
+import gen.proto.python.echoapis.echo.v1 as v1
+
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+PORT = 65432  # The port used by the server
 
 '''Simple python client'''
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    echov1.EchoRequest
-    data = s.recv(1024)
 
-print('Received', repr(data))
+async def main() -> None:
+    channel = Channel(host=HOST, port=PORT)
+    service = v1.EchoServiceStub(channel)
 
+    response = await service.echo(msg="Nate")
+    print(f"echo {response.msg=}")
+    response = await service.echo_many(msg="Yada", repeat=3)
+    print(f"echo many {response.msg=}")
+    # don't forget to close the channel when done!
+    channel.close()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
